6625b44478a5ae6bedeae998408e9750ab0a121e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-21 23:43:01 +0100

    Flytt data til separat fil

--- src/App.js
+++ src/App.js
@@ -4,46 +4,10 @@ import Header from "./components/Header";
 import ContainerOptions from "./components/ContainerOptions";
 import ContainerSelected from "./components/ContainerSelected";
 
+import { handicaps } from "./data/handicaps";
+
 export default function App() {
-  const [options, setOptions] = useState([
-    {
-      id: 1,
-      option: "Remove f7 pawn from play",
-      action: false,
-    },
-    {
-      id: 2,
-      option: "Remove b1/b8 knight from play",
-      action: false,
-    },
-    {
-      id: 3,
-      option: "Checkmate must be delivered with a rook",
-      action: false,
-    },
-    {
-      id: 4,
-      option: "Checkmate must be delivered with at least one a-b-c-d pawn",
-      action: false,
-    },
-    {
-      id: 5,
-      option: "Checkmate must be delivered on d-file",
-      action: false,
-    },
-    {
-      id: 6,
-      option:
-        "The weaker player's king may move up to two squares in any direction in a straight line",
-      action: false,
-    },
-    {
-      id: 7,
-      option:
-        "When moving a pawn, roll a dice. On a result of 1-2, revert the move",
-      action: false,
-    },
-  ]);
+  const [options, setOptions] = useState(handicaps);
   const [selected, setSelected] = useState([]);
 
   return (
